82245cc455213031197e0b2890661c7a0131bc9c
jakeogh <github.com@v6y.net>
2019-01-13 03:39:43 -0700

    auto-commit

--- gpgmda-client-send.sh
+++ gpgmda-client-send.sh
@@ -1,38 +1,23 @@
 #!/bin/bash
 
 #
-# 1. pipe stdin to sendmail on the mailserver over ssh (this delivers the message to all recipiente)
-# 2. pipe the same message directly to the remote mda using the "sentuser" (this puts a exact message copy in the sentuser gpgmda/.sent on the server, keeping the BCC fields)
-#   * the order is actually reversed, so if something goes wrong, you have the message you were trying to send
+# 1. take stdin and tee it to /dev/shm/lastmail
+# 2. pipe stdin to ssh user@domain.com
+# 3. pee on the server side to the sentusers maildir and send the message via sendmail
 
 # note the send from address $1 (fed to sendmail) needs to match the ssh login and therefore the local user on the mailserver
 
-
 # $1 sets the user@email_server.com that this mail is submitted to via ssh for sending via sendmail
 # $2 sets the Sender: (not the From:, that is set when you compose the message)
-
-#echo "$1" " " "$2" > /home/user/delthissss 2>&1
 
 user="${1}"
 shift
 domain=`echo "${user}" | cut -d '@' -f 2`
 sentuser="sentuser@${domain}"
 
-#cat - |  ssh "${1}" "cat - | /usr/sbin/sendmail -t -i -f ${2}" || exit 1
-#cat - | tee /dev/shm/lastmail | ssh "${1}" "cat - | /usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}" || exit 1
-
-#bug potential race
-#bug uploading the message twice (cant write it to the remote disk, need to pee |)
-
-# user@mail0:~$ sudo -u sentuser -g sentuser -i /bin/bash -c "/bin/cat /home/user/testmail | /bin/gpgmda"
-
-#tee /dev/shm/lastmail | \
-#    ssh "${user}" "pee sudo -u sentuser -g sentuser -i /bin/bash -c '/bin/cat - | /bin/gpgmda' '/usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}'"
-
-
 tee /dev/shm/lastmail | \
     /usr/bin/ssh "${user}" "pee \"sudo -u sentuser -g sentuser -i /bin/bash -c '/bin/cat - | /bin/gpgmda'\" \"/usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}\""
 
-#working:
+#old working:
 #tee /dev/shm/lastmail | ssh "${sentuser}" "cat - | /bin/gpgmda" || exit 1
 #cat /dev/shm/lastmail | ssh "${user}" "cat - | /usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}" || exit 1
