112a107d4b2c5b04d6bd34f563e2b1cdae119f4a
jakeogh <github.com@v6y.net>
2019-01-13 01:59:20 -0700

    auto-commit

--- gpgmda-client-send.sh
+++ gpgmda-client-send.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-# pipe stdin to sendmail on the mailserver over ssh
-# note the send from address needs to match the ssh login and therefore the local user on the mailserver
-# this script needs improvement, there is no guarantee it sent... and BCC is stripped
-# also, always_bcc=sentuser is required in main.cf or sent mail wont show up at all. ugly.
+#
+# 1. pipe stdin to sendmail on the mailserver over ssh (this delivers the message to all recipiente)
+# 2. pipe the same message directly to the remote mda using the "sentuser" (this puts a exact message copy in the sentuser gpgmda/.sent on the server, keeping the BCC fields)
+#   * the order is actually reversed, so if something goes wrong, you have the message you were trying to send
+
+# note the send from address $1 (fed to sendmail) needs to match the ssh login and therefore the local user on the mailserver
+
 
 # $1 sets the user@email_server.com that this mail is submitted to via ssh for sending via sendmail
 # $2 sets the Sender: (not the From:, that is set when you compose the message)
 
 #echo "$1" " " "$2" > /home/user/delthissss 2>&1
 
-domain=`echo "${1}" | cut -d '@' -f 2`
+user="${1}"
+shift
+domain=`echo "${user}" | cut -d '@' -f 2`
+sentuser="sentuser@${domain}"
 
 #cat - |  ssh "${1}" "cat - | /usr/sbin/sendmail -t -i -f ${2}" || exit 1
 #cat - | tee /dev/shm/lastmail | ssh "${1}" "cat - | /usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}" || exit 1
-tee /dev/shm/lastmail | ssh "${1}" "cat - | /usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}" || exit 1
+
+#bug potential race
+tee /dev/shm/lastmail | ssh "${sentuser}" "cat - | /bin/gpgmda" || exit 1
+cat /dev/shm/lastmail | ssh "${user}" "cat - | /usr/sbin/sendmail -N delay,failure,success -t -i -f reply_to_the_from_address@${domain}" || exit 1
